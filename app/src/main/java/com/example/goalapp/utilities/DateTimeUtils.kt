c09cc541e8303bf949dbd2a9e07277fd69396257
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-07-12 16:29:57 +0300

    Update DateTimeUtils

--- app/src/main/java/com/example/goalapp/utilities/DateTimeUtils.kt
+++ app/src/main/java/com/example/goalapp/utilities/DateTimeUtils.kt
@@ -13,7 +13,14 @@ fun localDateTimeToLong(localDateTime: LocalDateTime, utc: Boolean = false): Lon
     return localDateTime.atZone(zone).toInstant().toEpochMilli()
 }
 
-fun longToLocalDateTime(value: Long): LocalDateTime {
+fun longToLocalDateTime(value: Long, utc: Boolean = false): LocalDateTime {
+    if (utc) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(value), TimeZone.getTimeZone(ZoneOffset.UTC.id).toZoneId())
+    }
     val zone = TimeZone.getDefault().toZoneId()
     return LocalDateTime.ofInstant(Instant.ofEpochMilli(value), zone)
+}
+
+fun utcLongNow(): Long {
+    return Instant.now().toEpochMilli()
 }
--- app/src/test/java/com/example/goalapp/DateTimeUtilTest.kt
+++ app/src/test/java/com/example/goalapp/DateTimeUtilTest.kt
@@ -4,7 +4,10 @@ import com.example.goalapp.utilities.localDateTimeToLong
 import com.example.goalapp.utilities.longToLocalDateTime
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class DateTimeUtilTest {
     @Test
@@ -14,5 +17,27 @@ class DateTimeUtilTest {
         val dtLong = localDateTimeToLong(dateTime)
         val dateTime2 = longToLocalDateTime(dtLong)
         assertEquals(dateTime, dateTime2)
+    }
+
+    @Test
+    fun test_localDateTimeToLongUTC() {
+        val localDate = LocalDate.of(2022, 7, 10)
+        val dateTime = localDate.atStartOfDay()
+        val dtLong = localDateTimeToLong(dateTime, true)
+        val dateTime2 = longToLocalDateTime(dtLong, true)
+        assertEquals(dateTime, dateTime2)
+    }
+
+    @Test
+    fun test_localDateTimeTimeZoneConversion() {
+        val localDateTime = LocalDateTime.of(2022, 7, 11, 12, 15)
+        val hoursOffset = 3
+        val offset = ZoneOffset.ofHours(hoursOffset)
+        val utcLong = localDateTime.atOffset(offset).toInstant().toEpochMilli()
+        val utcLocalDateTime = localDateTime.minusHours(hoursOffset.toLong())
+        assertEquals(utcLocalDateTime, longToLocalDateTime(utcLong, true))
+        assertEquals(utcLong, localDateTimeToLong(utcLocalDateTime, true))
+        val localDateTime2 = LocalDateTime.ofInstant(Instant.ofEpochMilli(utcLong), offset)
+        assertEquals(localDateTime, localDateTime2)
     }
 }
