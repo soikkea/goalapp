329a97cfc2169b03b8f3b89223c9dd9ed99e4f63
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-07-14 18:08:13 +0300

    Start work on GoalDetailsScreen

--- app/src/main/java/com/example/goalapp/ui/details/GoalDetailsScreen.kt
+++ app/src/main/java/com/example/goalapp/ui/details/GoalDetailsScreen.kt
@@ -1,6 +1,8 @@
 package com.example.goalapp.ui.details
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -8,39 +10,95 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.goalapp.R
+import com.example.goalapp.data.Goal
+import com.example.goalapp.data.GoalWithProgress
 import com.example.goalapp.ui.components.TopAppBarWithBackButton
+import com.example.goalapp.ui.theme.GoalAppTheme
+import com.example.goalapp.viewmodels.GoalDetailsViewModel
+import java.time.LocalDate
 
 @Composable
 fun GoalDetailsScreen(
-    goalId: Int,
-    onBack: () -> Unit
+    onBack: () -> Unit,
+    viewModel: GoalDetailsViewModel
 ) {
-    val openDialog = remember {
+    var openDialog by remember {
         mutableStateOf(false)
     }
+    val today = LocalDate.now()
+    val goal by viewModel.goal.observeAsState()
+    GoalDetailsScaffold(onBack, { openDialog = true }, goal, today)
+    if (openDialog) {
+        // TODO
+        //AddProgressDialog(onClose = { openDialog.value = false })
+    }
+}
+
+@Composable
+private fun GoalDetailsScaffold(
+    onBack: () -> Unit,
+    onFABClick: () -> Unit,
+    goal: GoalWithProgress?,
+    currentDate: LocalDate
+) {
     Scaffold(
         topBar = {
             TopAppBarWithBackButton(
-                title = "Details",
+                title = stringResource(id = R.string.details),
                 onBack = onBack
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { openDialog.value = true }) {
-                Icon(Icons.Default.Add, contentDescription = "Add progress")
+            FloatingActionButton(onClick = onFABClick) {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.add_progress)
+                )
             }
         }
     ) { contentPadding ->
-        Column(modifier = Modifier.padding(contentPadding)) {
-            Text(text = "$goalId")
+        if (goal != null) {
+            GoalDetailContent(contentPadding, goal)
         }
-
     }
-    if (openDialog.value) {
-        AddProgressDialog(onClose = { openDialog.value = false })
+}
+
+@Composable
+private fun GoalDetailContent(
+    contentPadding: PaddingValues,
+    goal: GoalWithProgress
+) {
+    Column(modifier = Modifier.padding(contentPadding)) {
+        Text(text = goal.goal.title)
+        Row() {
+            Text(text = stringResource(id = R.string.start_date))
+            Text(text = goal.goal.startDate.toString())
+        }
+        Row() {
+            Text(text = stringResource(id = R.string.end_date))
+            Text(text = goal.goal.endDate.toString())
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    val today = LocalDate.of(2022, 7, 1)
+    val goal = Goal.create("Test Goal", today, today.plusDays(6), 7)
+    val gwp = GoalWithProgress(goal, emptyList())
+    GoalAppTheme {
+        GoalDetailsScaffold(
+            onBack = { /*TODO*/ },
+            onFABClick = { /*TODO*/ },
+            goal = gwp,
+            currentDate = today
+        )
     }
 }
