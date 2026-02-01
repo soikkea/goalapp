3dd84002c351315c80bcbd5ac43f49302529a710
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2026-02-01 13:31:50 +0200

    Update AGP to 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.hilt.android) apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
+    id("com.google.devtools.ksp") version "2.3.2"
 }
